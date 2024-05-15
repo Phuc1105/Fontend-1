52b6e594f816e8247c6795b6873c3a6ff6e827c5
Phuc1105 <phucpvpc06866@fpt.edu.vn>
2024-05-15 16:01:28 +0700

    [angular]-routing phân trang

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -3,6 +3,10 @@ import {NgModule} from '@angular/core';
 
 import {PagesComponent} from './pages.component';
 import {DashboardComponent} from "./dashboard/dashboard.component";
+import { UsersComponent } from 'app/users/users.component';
+import { ProductComponent } from 'app/product/product.component';
+import { CategoryComponent } from 'app/category/category.component';
+import { InventoryComponent } from 'app/inventory/inventory.component';
 
 const routes: Routes = [{
   path: '',
@@ -12,6 +16,24 @@ const routes: Routes = [{
       path: 'dashboard',
       component: DashboardComponent,
       data: {breadcrumb: 'Dashboard'},
+    },
+    {
+      path: 'users',
+      component: UsersComponent,
+      data: {breadcrumb: 'Users'},
+    },
+    {
+      path: 'product',
+      component: ProductComponent,
+      data: {breadcrumb: 'Product'},
+    },{
+      path: 'category',
+      component: CategoryComponent,
+      data: {breadcrumb: 'Category'},
+    },{
+      path: 'inventory',
+      component: InventoryComponent,
+      data: {breadcrumb: 'Inventory'},
     },
   ],
 }];
