fddcecc727c27de6c5365104ef9567235e4d198c
LamChiThoai2223 <132965868+LamChiThoai2223@users.noreply.github.com>
2024-05-16 20:44:33 +0700

    Merge commit '6faf340aa7ad9b4fdcd1115ee3dd7d45ff3e3818' into dev/thoai

---
+++ src/app/pages/pages-routing.module.ts
@@ -0,0 +1,58 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { PagesComponent } from './pages.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AddUserComponent } from './users/add-user/add-user.component';
+import { ListUsersComponent } from './users/list-users/list-users.component';
+import { AddProductComponent } from './products/add-product/add-product.component';
+import { ListProductsComponent } from './products/list-products/list-products.component';
+import { AddCategoryComponent } from './categories/add-category/add-category.component';
+import { ListCategoriesComponent } from './categories/list-categories/list-categories.component';
+import { ListInventoryComponent } from './inventories/list-inventory/list-inventory.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: PagesComponent,
+    children: [
+      {
+        path: 'dashboard',
+        component: DashboardComponent,
+      },
+      {
+        path: 'users/add-user',
+        component: AddUserComponent,
+      },
+      {
+        path: 'users/list-users',
+        component: ListUsersComponent,
+      },
+      {
+        path: 'products/add-product',
+        component: AddProductComponent,
+      },
+      {
+        path: 'products/list-products',
+        component: ListProductsComponent,
+      },
+      {
+        path: 'categories/add-category',
+        component: AddCategoryComponent,
+      },
+      {
+        path: 'categories/list-categories',
+        component: ListCategoriesComponent,
+      },
+      {
+        path: 'inventories/list-inventory',
+        component: ListInventoryComponent,
+      },
+    ],
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class PagesRoutingModule {}
---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,37 @@
+import {NgModule} from '@angular/core';
+import {NbMenuModule} from "@nebular/theme";
+import {ThemeModule} from '../@theme/theme.module';
+
+import {PagesComponent} from './pages.component';
+import {DashboardModule} from './dashboard/dashboard.module';
+import {PagesRoutingModule} from './pages-routing.module';
+import {PaginatorModule} from "../@theme/components/paginator/paginator.module";
+import { AddUserComponent } from './users/add-user/add-user.component';
+import { ListUsersComponent } from './users/list-users/list-users.component';
+import { AddProductComponent } from './products/add-product/add-product.component';
+import { ListProductsComponent } from './products/list-products/list-products.component';
+import { AddCategoryComponent } from './categories/add-category/add-category.component';
+import { ListCategoriesComponent } from './categories/list-categories/list-categories.component';
+import { ListInventoryComponent } from './inventories/list-inventory/list-inventory.component';
+
+@NgModule({
+  imports: [
+    PagesRoutingModule,
+    ThemeModule,
+    DashboardModule,
+    NbMenuModule,
+    PaginatorModule,
+  ],
+  declarations: [
+    PagesComponent,
+    AddUserComponent,
+    ListUsersComponent,
+    AddProductComponent,
+    ListProductsComponent,
+    AddCategoryComponent,
+    ListCategoriesComponent,
+    ListInventoryComponent,
+  ],
+  providers: []
+})
+export class PagesModule { }
