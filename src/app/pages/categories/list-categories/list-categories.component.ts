9ffad51f2e28cad1daa302d5f27952897c899a91
LamChiThoai2223 <132965868+LamChiThoai2223@users.noreply.github.com>
2024-05-18 19:41:43 +0700

    [Angular] - Thêm dữ liệu cho bảng

--- src/app/pages/categories/list-categories/list-categories.component.ts
+++ src/app/pages/categories/list-categories/list-categories.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./list-categories.component.scss']
 })
 export class ListCategoriesComponent {
-
+  categories = [
+    { id: 1, name: 'Trà trái cây tươi', description: 'Ngon Quá Ngon Quá', status: 'Kích hoạt', trend: true, image: 'assets/images/maxresdefault.jpg' },
+    { id: 2, name: 'Trà sữa 3 vị', description: 'Độc lạ', status: 'Không kích hoạt', trend: false, image: 'assets/images/category2.jpg' },
+    { id: 3, name: 'Matcha', description: 'Ngon', status: 'Kích hoạt', trend: true, image: 'assets/images/category3.jpg' }
+  ];
+  
 }
