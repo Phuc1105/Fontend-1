d0b039e146b968770ec72ea974e062ad3e5d391f
Phuc1105 <phucpvpc06866@fpt.edu.vn>
2024-05-29 14:16:25 +0700

    [angular]-[html-css]-bắt lỗi form giao hàng và danh sách

--- src/app/pages/delivery/add-delivery/add-delivery.component.ts
+++ src/app/pages/delivery/add-delivery/add-delivery.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-delivery',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-delivery.component.scss']
 })
 export class AddDeliveryComponent {
+  orderForm!: FormGroup;
 
+  constructor() {}
+  
+  ngOnInit(): void {
+    this.orderForm = new FormGroup({
+      customer_name: new FormControl('', [Validators.required]),
+      customer_phone: new FormControl('', [Validators.required, Validators.pattern("^0[0-9]{9}$")]),
+     
+    });
+  }
+  onSubmit(){
+    if(this.orderForm.valid){
+      console.log(this.orderForm.value);
+    }else{
+      console.log('Form is not valid');
+      
+    }
+    
+  }
 }
