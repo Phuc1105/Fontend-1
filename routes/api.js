3ebee527f6b43581f3384fb82c2508183030df3c
NguyenTrungToan <nguyentrungtoan.2004@gmail.com>
2024-06-10 14:48:42 +0700

    [api]

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const usersApiControllers = require('../controllers/api/users');
+const  personnelApiControllers = require('../controllers/api/personnels')
 const router = express.Router();
 // ============== Api User ================
 router.get('/users',usersApiControllers.getUsers);
--- server.js
+++ server.js
@@ -2,15 +2,15 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const apiRouter = require('./routes/api');
-const productRouter = require('./products');
-const userRouter = require('./users')
+// const productRouter = require('./products');
+// const userRouter = require('./users')
 
 app.use(express.json()); 
 app.use(cors()); 
 
 app.use('/api', apiRouter);
-app.use('/api',productRouter)
-app.use('/api', userRouter);
+// app.use('/api',productRouter)
+// app.use('/api', userRouter);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
